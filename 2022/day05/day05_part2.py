8b3417b42ffd0dca6f8f7cc695d90b5fd3bdd3df
Kurt Schwind <Kurt.Schwind@gmail.com>
2023-11-23 08:03:23 -0600

    2022 Day 05

---
+++ 2022/day05/day05_part2.py
@@ -0,0 +1,61 @@
+#!/usr/local/bin/python3
+def stack_count(l):
+    return 9
+    # return int((len(l) - 1) / 3)
+
+
+def com_tuple(l):
+    _, a, _, b, _, c = l.split()
+    return (int(a), int(b) - 1, int(c) - 1)
+
+
+def do_command(c, stacks):
+    qty, src, tgt = c
+    temp = []
+    for j in range(qty):
+        temp.append(stacks[src].pop())
+
+    temp.reverse()
+    for x in temp:
+        stacks[tgt].append(x)
+
+    return stacks
+
+
+def main():
+    # infile = "sample.txt"
+    infile = "input.txt"
+
+    with open(infile) as f:
+        lines = f.readlines()
+
+    num_stacks = stack_count(lines[0])
+    print("Num Stacks : {}".format(num_stacks))
+    stack_lines = [x for x in lines if "[" in x]
+    commands = [x for x in lines if x[0] == "m"]
+    stacks = [[] for s in range(num_stacks)]
+
+    # Populate stacks
+
+    for l in stack_lines:
+        for x in range(num_stacks):
+            idx = 3 * x + (x + 1)
+            if l[idx] != " ":
+                stacks[x].append(l[idx])
+
+    for x in range(num_stacks):
+        stacks[x].reverse()
+    print(stacks)
+
+    print(commands)
+
+    for c in commands:
+        stacks = do_command(com_tuple(c), stacks)
+    print(stacks)
+
+    for j in stacks:
+        print(j.pop())
+
+
+if __name__ == "__main__":
+    main()
