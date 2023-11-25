40104b6057795363cecfd09ae4e0655926b72009
Kurt Schwind <Kurt.Schwind@gmail.com>
2023-11-25 12:39:28 -0600

    Working on roll out script

--- 2023/advent.sh
+++ 2023/advent.sh
@@ -6,4 +6,11 @@ then
 	exit 
 fi
 
-cargo new --lib day-$1
+export SRCLIB=day-00
+export LIBNAME=day-$1
+
+cargo new --lib $LIBNAME
+cp $SRCLIB/solve.sh $LIBNAME/solve.sh
+mkdir $LIBNAME/src/bin/
+cp -v $SRCLIB/src/bin/part-1.rs $LIBNAME/src/bin/
+cp -v $SRCLIB/src/bin/part-2.rs $LIBNAME/src/bin/
