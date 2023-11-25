92ffcd75909f32fc9434b70a3f536a2fe6e99d80
Kurt Schwind <Kurt.Schwind@gmail.com>
2023-11-25 13:03:16 -0600

    Working on roll out script

--- 2023/advent.sh
+++ 2023/advent.sh
@@ -12,5 +12,9 @@ export LIBNAME=day-$1
 cargo new --lib $LIBNAME
 cp $SRCLIB/solve.sh $LIBNAME/solve.sh
 mkdir $LIBNAME/src/bin/
-cp -v $SRCLIB/src/bin/part-1.rs $LIBNAME/src/bin/
-cp -v $SRCLIB/src/bin/part-2.rs $LIBNAME/src/bin/
+sed "s/00/$1/g" $SRCLIB/src/bin/part-1.rs > $LIBNAME/src/bin/part-1.rs
+sed "s/00/$1/g" $SRCLIB/src/bin/part-2.rs $LIBNAME/src/bin/part-2.rs
+cp -v $SRCLIB/src/lib.rs $LIBNAME/src/lib.rs
+
+cd $LIBNAME
+cargo test
