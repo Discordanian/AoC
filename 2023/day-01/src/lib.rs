acca973105e9b05354d0fc2b2503eb2f4a7fd950
Kurt Schwind <kurt.schwind@gmail.com>
2023-12-01 08:46:22 -0600

    Part 2 complete

--- 2023/day-01/src/lib.rs
+++ 2023/day-01/src/lib.rs
@@ -21,15 +21,14 @@ pub fn process_part1(input: &str) -> String {
 
 pub fn process_part2(input: &str) -> String {
     let mut new_input : String = String::from(input);
-    new_input = new_input.replace("zerone","0");
-    new_input = new_input.replace("nineight","9");
-    new_input = new_input.replace("sevenine","7");
-    new_input = new_input.replace("fiveight","5");
-    new_input = new_input.replace("threeight","3");
-    new_input = new_input.replace("eightwo","8");
-    new_input = new_input.replace("eighthree","8");
-    new_input = new_input.replace("twone","2");
-    new_input = new_input.replace("oneight","1");
+    new_input = new_input.replace("nineight","98");
+    new_input = new_input.replace("sevenine","79");
+    new_input = new_input.replace("oneight","18");
+    new_input = new_input.replace("eightwo","82");
+    new_input = new_input.replace("eighthree","83");
+    new_input = new_input.replace("fiveight","58");
+    new_input = new_input.replace("threeight","38");
+    new_input = new_input.replace("twone","21");
     new_input = new_input.replace("one","1");
     new_input = new_input.replace("two","2");
     new_input = new_input.replace("three","3");
@@ -39,31 +38,9 @@ pub fn process_part2(input: &str) -> String {
     new_input = new_input.replace("seven","7");
     new_input = new_input.replace("eight","8");
     new_input = new_input.replace("nine","9");
-    // new_input = new_input.replace("zero","0");
-    println!("{}", new_input);
 
-    let mut first = true;
-    let mut sum = 0;
-    let mut last_char = '0';
-    let mut first_char = '0';
-    for c in new_input.as_str().chars() {
-        if c == '\n' {
-            first = true;
-            sum += last_char.to_digit(10).unwrap();
-            println!("Newline, add character {}{}", first_char,last_char);
-        }
-        if c.is_digit(10) && !first {
-            last_char = c;
-        }
-        if c.is_digit(10) && first {
-            // println!("First digit add 10 * character {}", c);
-            first_char = c;
-            sum += c.to_digit(10).unwrap() * 10;
-            last_char = c;
-            first = false;
-        }
-    }
-    format!("{}",sum)
+    process_part1(new_input.as_str())
+
 }
 
 #[cfg(test)]
