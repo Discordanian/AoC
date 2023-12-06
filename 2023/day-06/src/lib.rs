4a767480b59cdc54abf6ab7e74708797a3c76ad1
Kurt Schwind <kurt.schwind@gmail.com>
2023-12-06 13:58:44 -0600

    day06_part1

--- 2023/day-06/src/lib.rs
+++ 2023/day-06/src/lib.rs
@@ -1,56 +1,107 @@
-pub fn process_part1(input: &str) -> String {
-    let result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    result.to_string()
+#[derive(Clone, Debug)]
+struct Race {
+    time: u32,
+    distance: u32,
+}
+
+/*
+impl From<&str> for Conversion {
+    fn from(line: &str) -> Self {
+
+        let parts: Vec<i32> = line
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+        Conversion {
+            dest_start: parts[0],
+            source_start: parts[1],
+            length: parts[2],
+        }
+    }
+}
+
+*/
+impl Race {
+    fn wins(&self) -> u32 {
+        (1..self.time)
+            .filter(|x| self.expected(*x) > self.distance)
+            .count() as u32
+    }
+    fn expected(&self, x: u32) -> u32 {
+        // note that this forms a parabola facing down with an apex at time/2
+        x * (self.time - x)
+    }
 }
 
 pub fn process_part2(input: &str) -> String {
-    let mut result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
-
-    result.sort_by(|a,b| b.cmp(a)); // reverse sort
-    let sum: u32 = result.iter().take(3).sum();
-    sum.to_string()
+    let lines: Vec<&str> = input.lines().collect();
+    let time: u64 = lines[0][5..] // Get rid of "Time:"
+        .chars()
+        .filter(|x| *x != ' ')
+        .fold(0_u64, |mut acc, x| {
+            acc *= 10;
+            acc += x.to_digit(10).expect("Not a number?") as u64;
+            acc
+        });
+    let distance: u64 = lines[1][9..] // Get rid of "Distance:"
+        .chars()
+        .filter(|x| *x != ' ')
+        .fold(0_u64, |mut acc, x| {
+            acc *= 10;
+            acc += x.to_digit(10).expect("Not a number?") as u64;
+            acc
+        });
+    (1..time)
+        .filter(|x| x * (time - x) > distance)
+        .count()
+        .to_string()
 }
+
+pub fn process_part1(input: &str) -> String {
+    let lines: Vec<&str> = input.lines().collect();
+    let times: Vec<u32> = lines[0]
+        .split_whitespace()
+        .skip(1)
+        .map(|x| x.trim().parse().unwrap())
+        .collect();
+    let distances: Vec<u32> = lines[1]
+        .split_whitespace()
+        .skip(1)
+        .map(|x| x.trim().parse().unwrap())
+        .collect();
+    let mut races: Vec<Race> = vec![];
+    for x in 0..times.len() {
+        races.push(Race {
+            time: times[x],
+            distance: distances[x],
+        });
+    }
+    races
+        .iter()
+        .map(|race| race.wins())
+        .product::<u32>()
+        .to_string()
+}
+
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT : &str = "1
-2
-
-5
-
-1
-
-3
-4";
+    const INPUT : &str = "Time:      7  15   30
+Distance:  9  40  200";
 
     #[test]
     fn part1_works() {
         let result = process_part1(INPUT);
-        assert_eq!(result, "7".to_string());
+        assert_eq!(result, "288".to_string());
     }
 
     #[test]
     fn part2_works() {
         let result = process_part2(INPUT);
-        assert_eq!(result, "15".to_string());
+        assert_eq!(result, "71503".to_string());
     }
 }
