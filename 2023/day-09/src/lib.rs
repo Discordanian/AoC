dcbe7f621a75b34446b3b23d7384d5097dfbc913
Kurt Schwind <kurt.schwind@gmail.com>
2023-12-09 05:40:30 -0600

    2023 day 09

---
+++ 2023/day-09/src/lib.rs
@@ -0,0 +1,87 @@
+/*
+lines = fin.read().strip().split("\n")
+
+
+def diff(arr):
+    return [arr[i+1] - arr[i] for i in range(len(arr) - 1)]
+
+
+def extrapolate(hist):
+    layers = [hist]
+
+    while not all([x == 0 for x in layers[-1]]):
+        layers.append(diff(layers[-1]))
+
+    layers[-1].append(0)
+    for i in range(len(layers) - 2, -1, -1):
+        layers[i].append(layers[i][-1] + layers[i+1][-1])
+
+    return layers[0][-1]
+
+
+ans = []
+for line in lines:
+    arr = list(map(int, line.split()))
+    ans.append(extrapolate(arr))
+
+sum(ans)
+ */
+pub fn process_part1(input: &str) -> u32 {
+    input.len() as u32
+}
+
+
+/*
+lines = fin.read().strip().split("\n")
+
+
+def diff(arr):
+    return [arr[i+1] - arr[i] for i in range(len(arr) - 1)]
+
+
+def extrapolate(hist):
+    layers = [hist]
+
+    while not all([x == 0 for x in layers[-1]]):
+        layers.append(diff(layers[-1]))
+
+    layers[-1].append(0)
+    for i in range(len(layers) - 2, -1, -1):
+        layers[i].append(layers[i][-1] + layers[i+1][-1])
+
+    return layers[0][-1]
+
+
+ans = []
+for line in lines:
+    arr = list(map(int, line.split()[::-1]))
+    ans.append(extrapolate(arr))
+
+sum(ans)
+*/
+
+pub fn process_part2(input: &str) -> u32 {
+    input.len() as u32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT : &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+";
+
+    #[test]
+    fn part1_works() {
+        let result = process_part1(INPUT);
+        assert_eq!(result, 114);
+    }
+
+    #[test]
+    fn part2_works() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, 2);
+    }
+}
