d4e09586b9d52671bbec751b1ef923a0eee6610d
Kurt Schwind <kurt.schwind@gmail.com>
2023-12-12 04:37:51 -0600

    Stub

--- 2023/day-11/src/lib.rs
+++ 2023/day-11/src/lib.rs
@@ -1,31 +1,103 @@
 pub fn process_part1(input: &str) -> u32 {
-    let result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    result
+
+/*
+with open("./input.txt") as fin:
+    lines = fin.read().strip().split("\n")
+
+n, m = len(lines), len(lines[0])
+
+coords = []
+for i in range(n):
+    for j in range(m):
+        if lines[i][j] == "#":
+            coords.append((i, j))
+
+N = len(coords)
+
+empty_row = [all([lines[i][j] == "." for j in range(m)]) for i in range(n)]
+empty_col = [all([lines[i][j] == "." for i in range(n)]) for j in range(m)]
+
+
+def dist(a, b):
+    i1, j1 = a
+    i2, j2 = b
+
+    i1, i2 = min(i1, i2), max(i1, i2)
+    j1, j2 = min(j1, j2), max(j1, j2)
+
+    ans = 0
+    for i in range(i1, i2):
+        ans += 1
+        if empty_row[i]:
+            ans += 1
+    for j in range(j1, j2):
+        ans += 1
+        if empty_col[j]:
+            ans += 1
+
+    return ans
+
+
+ans = 0
+for idx1 in range(N):
+    for idx2 in range(idx1+1, N):
+        d = dist(coords[idx1], coords[idx2])
+        ans += d
+
+print(ans)
+*/
+    0
 }
 
 pub fn process_part2(input: &str) -> u32 {
-    let mut result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
+/*
 
-    result.sort_by(|a,b| b.cmp(a)); // reverse sort
-    let sum: u32 = result.iter().take(3).sum();
-    sum
+with open("./input.txt") as fin:
+    lines = fin.read().strip().split("\n")
+
+n, m = len(lines), len(lines[0])
+
+coords = []
+for i in range(n):
+    for j in range(m):
+        if lines[i][j] == "#":
+            coords.append((i, j))
+
+N = len(coords)
+
+empty_row = [all([lines[i][j] == "." for j in range(m)]) for i in range(n)]
+empty_col = [all([lines[i][j] == "." for i in range(n)]) for j in range(m)]
+
+
+def dist(a, b):
+    i1, j1 = a
+    i2, j2 = b
+
+    i1, i2 = min(i1, i2), max(i1, i2)
+    j1, j2 = min(j1, j2), max(j1, j2)
+
+    ans = 0
+    for i in range(i1, i2):
+        ans += 1
+        if empty_row[i]:
+            ans += 10**6 - 1
+    for j in range(j1, j2):
+        ans += 1
+        if empty_col[j]:
+            ans += 10**6 - 1
+
+    return ans
+
+
+ans = 0
+for idx1 in range(N):
+    for idx2 in range(idx1+1, N):
+        d = dist(coords[idx1], coords[idx2])
+        ans += d
+
+print(ans)
+*/    
+    0
 }
 
 #[cfg(test)]
