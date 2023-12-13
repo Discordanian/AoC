5275a69f2d6c7330e7a7fcb511ca54c85c0faf50
Kurt Schwind <kurt.schwind@gmail.com>
2023-12-13 08:46:38 -0600

    Day13p1

--- 2023/day-13/src/lib.rs
+++ 2023/day-13/src/lib.rs
@@ -1,3 +1,23 @@
+pub fn transpose(matrix: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    if matrix.is_empty() {
+        return Vec::new();
+    }
+    let num_rows = matrix.len();
+    let num_cols = matrix[0].len();
+
+    let mut transposed = Vec::with_capacity(num_cols);
+    for _ in 0..num_cols {
+        transposed.push(Vec::with_capacity(num_rows));
+    }
+
+    for i in 0..num_rows {
+        for j in 0..num_cols {
+            transposed[j].push(matrix[i][j]);
+        }
+    }
+    transposed
+}
+
 pub fn process_part1(input: &str) -> u32 {
     let fields: Vec<Vec<Vec<char>>> = input
         .split("\n\n")
@@ -8,6 +28,7 @@ pub fn process_part1(input: &str) -> u32 {
 }
 
 pub fn process_part2(input: &str) -> u32 {
+    // 'smudge' a cell and if there is no reflection, smucde the next cell and so on.
     0
 }
 
