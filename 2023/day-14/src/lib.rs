b7f5af65aa4fac50ce9ca65ef9571fd738f68a1e
Kurt Schwind <Kurt.Schwind@gmail.com>
2023-12-14 12:32:53 -0600

    Scoring

--- 2023/day-14/src/lib.rs
+++ 2023/day-14/src/lib.rs
@@ -1,56 +1,68 @@
 pub fn process_part1(input: &str) -> u32 {
-    let result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    result
+    input.len() as u32
+}
+
+pub fn score_input(input: &str) -> u32 {
+    let matrix: Vec<Vec<char>> = input.lines().collect::<Vec<&str>>().iter().map(|line| line.chars().collect()).collect();
+    score_grid(&matrix)
+}
+
+pub fn score_grid(matrix: &Vec<Vec<char>>) -> u32 {
+    let row_count: u32 = matrix.len() as u32;
+    matrix
+        .iter()
+        .enumerate()
+        .map(|(i, &ref row)|
+            (row_count - i as u32) * (row.iter().filter(|&&x| x=='O').count() as u32)
+        ).sum::<u32>()
 }
 
 pub fn process_part2(input: &str) -> u32 {
-    let mut result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
-
-    result.sort_by(|a,b| b.cmp(a)); // reverse sort
-    let sum: u32 = result.iter().take(3).sum();
-    sum
+    input.len() as u32
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT : &str = "1
-2
-
-5
-
-1
-
-3
-4";
+    const INPUT : &str = "O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#....
+";
 
     #[test]
     fn part1_works() {
         let result = process_part1(INPUT);
-        assert_eq!(result, 7);
+        assert_eq!(result, 136);
+    }
+
+    const INPUT_SCORE : &str = "OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#....";
+
+    #[test]
+    fn part1_score() {
+        let result = score_input(INPUT_SCORE);
+        assert_eq!(result, 136);
     }
 
     #[test]
     fn part2_works() {
         let result = process_part2(INPUT);
-        assert_eq!(result, 15);
+        assert_eq!(result, 64);
     }
 }
