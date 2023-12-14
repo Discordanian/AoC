8db256c5ad89e2bcd22e72ed57a5535a2ee5c661
Kurt Schwind <Kurt.Schwind@gmail.com>
2023-12-14 14:48:33 -0600

    2017 and 2018

--- 2016/day-01/src/lib.rs
+++ 2016/day-01/src/lib.rs
@@ -1,56 +1,28 @@
 pub fn process_part1(input: &str) -> u32 {
-    let result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    result
+    let instructions: Vec<&str> = input.split(", ").collect();
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    instructions.len() as u32
 }
 
 pub fn process_part2(input: &str) -> u32 {
-    let mut result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
-
-    result.sort_by(|a,b| b.cmp(a)); // reverse sort
-    let sum: u32 = result.iter().take(3).sum();
-    sum
+    let instructions: Vec<&str> = input.split(", ").collect();
+    instructions.len() as u32
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT : &str = "1
-2
-
-5
-
-1
-
-3
-4";
-
     #[test]
     fn part1_works() {
-        let result = process_part1(INPUT);
-        assert_eq!(result, 7);
+        let result = process_part1("R2, L3");
+        assert_eq!(result, 5);
     }
 
     #[test]
     fn part2_works() {
-        let result = process_part2(INPUT);
-        assert_eq!(result, 15);
+        let result = process_part2("R2, R2, R2");
+        assert_eq!(result, 2);
     }
 }
