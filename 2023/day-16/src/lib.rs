fa905d025187a3e755f37c645323e8da3e127a05
Kurt Schwind <Kurt.Schwind@gmail.com>
2023-12-16 08:37:59 -0600

    Closer

--- 2023/day-16/src/lib.rs
+++ 2023/day-16/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Copy, Debug, Clone)]
+#[derive(Copy, Debug, Clone, PartialEq, Eq)]
 pub enum Direction {
     East = 0,
     South = 1,
@@ -6,15 +6,103 @@ pub enum Direction {
     North = 3,
 }
 
-#[derive(Copy, Debug, Clone)]
+#[derive(Copy, Debug, Clone, PartialEq, Eq)]
 pub struct Ray {
     pos: (usize, usize),
     dir: Direction,
 }
 
+impl Ray {
+    fn next_ray(&self, matrix: &Vec<Vec<char>>) -> Vec<Ray> {
+        let mut retval = vec![];
+        let (x,y) = self.pos;
+        let elem = matrix[y][x];
+        let row_count = matrix.len();
+        let col_count = matrix[0].len();
+
+        let ns = (self.dir == Direction::North || self.dir == Direction::South);
+
+        if elem == '.' {
+            match self.dir {
+                Direction::North => retval.extend(self.north()),
+                Direction::East => retval.extend(self.east(col_count)),
+                Direction::South => retval.extend(self.south(row_count)),
+                Direction::West => retval.extend(self.west()),
+            }
+        }
+        if elem == '-' && ns {
+            retval.extend(self.west());
+            retval.extend(self.east(col_count));
+        }
+        if elem == '|' && !ns {
+            retval.extend(self.north());
+            retval.extend(self.south(row_count));
+        }
+        if elem == '/' {
+            match self.dir {
+                Direction::North => retval.extend(self.east(col_count)),
+                Direction::East => retval.extend(self.north()),
+                Direction::South => retval.extend(self.west()),
+                Direction::West => retval.extend(self.south(row_count)),
+            }
+        }
+        if elem == '\\' {
+            match self.dir {
+                Direction::North => retval.extend(self.west()),
+                Direction::East => retval.extend(self.south(row_count)),
+                Direction::South => retval.extend(self.east(col_count)),
+                Direction::West => retval.extend(self.north()),
+            }
+        }
+
+
+        retval
+
+    }
+    fn north(&self) -> Vec<Ray> {
+        if self.pos.1 > 0 {
+            return vec![Ray {pos: (self.pos.0, self.pos.1 - 1), dir: Direction::North}];
+        } 
+        vec![]
+    }
+    fn west(&self) -> Vec<Ray> {
+        if self.pos.0 > 0 {
+            return vec![Ray {pos: (self.pos.0 -1, self.pos.1), dir: Direction::West}];
+        } 
+        vec![]
+    }
+    fn east(&self,wall:usize) -> Vec<Ray> {
+        if self.pos.0 < wall - 1 {
+            return vec![Ray {pos: (self.pos.0 -1, self.pos.1), dir: Direction::East}];
+        } 
+        vec![]
+    }
+    fn south(&self,wall:usize) -> Vec<Ray> {
+        if self.pos.1 < wall - 1 {
+            return vec![Ray {pos: (self.pos.0, self.pos.1 + 1), dir: Direction::South}];
+        } 
+        vec![]
+    }
+}
+
 pub fn process_part1(input: &str) -> u32 {
     let matrix: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
     let mut rays: Vec<Ray> = vec![Ray {pos: (0,0),dir: Direction::East}];
+    // let mut seen: Vec<(usize, usize)> = Vec::new();
+    let mut past_rays: Vec<Ray> = Vec::new();
+
+    past_rays.push(rays[0]);
+
+
+    while !rays.is_empty() {
+        let ray = rays.pop().unwrap();
+        if past_rays.contains(&ray) {
+            continue;
+        }
+        past_rays.push(ray);
+        rays.extend(ray.next_ray(&matrix));
+    }
+    dbg!(&rays);
     matrix[0].len() as u32 + rays.len() as u32
 }
 
