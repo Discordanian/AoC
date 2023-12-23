14a75192e974e78beaab3f33923445550ac084e8
Kurt Schwind <kurt.schwind@gmail.com>
2023-12-23 06:39:19 -0600

    day23

--- 2023/day-23/src/lib.rs
+++ 2023/day-23/src/lib.rs
@@ -1,30 +1,8 @@
 pub fn process_part1(input: &str) -> u32 {
-    let result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
     result
 }
 
 pub fn process_part2(input: &str) -> u32 {
-    let mut result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
-
-    result.sort_by(|a,b| b.cmp(a)); // reverse sort
-    let sum: u32 = result.iter().take(3).sum();
     sum
 }
 
@@ -32,15 +10,29 @@ pub fn process_part2(input: &str) -> u32 {
 mod tests {
     use super::*;
 
-    const INPUT : &str = "1
-2
-
-5
-
-1
-
-3
-4";
+    const INPUT : &str = "#.#####################
+#.......#########...###
+#######.#########.#.###
+###.....#.>.>.###.#.###
+###v#####.#v#.###.#.###
+###.>...#.#.#.....#...#
+###v###.#.#.#########.#
+###...#.#.#.......#...#
+#####.#.#.#######.#.###
+#.....#.#.#.......#...#
+#.#####.#.#.#########v#
+#.#...#...#...###...>.#
+#.#.#v#######v###.###v#
+#...#.>.#...>.>.#.###.#
+#####v#.#.###v#.#.###.#
+#.....#...#...#.#.#...#
+#.#########.###.#.#.###
+#...###...#...#...#.###
+###.###.#.###v#####v###
+#...#...#.#.>.>.#.>.###
+#.###.###.#.###.#.#v###
+#.....###...###...#...#
+#####################.#";
 
     #[test]
     fn part1_works() {
