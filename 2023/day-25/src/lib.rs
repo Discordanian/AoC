bf086d51cf8d4cd616e4174f95e3e37cbb3a0f5f
Kurt Schwind <Kurt.Schwind@gmail.com>
2023-12-25 05:42:20 -0600

    Day25 Parse

--- 2023/day-25/src/lib.rs
+++ 2023/day-25/src/lib.rs
@@ -1,56 +1,50 @@
+use std::collections::{HashMap, HashSet};
+
 pub fn process_part1(input: &str) -> u32 {
-    let result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    result
+	let nodelist = parse_input(input);
+	dbg!(nodelist);
+	input.len() as u32
 }
 
-pub fn process_part2(input: &str) -> u32 {
-    let mut result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
 
-    result.sort_by(|a,b| b.cmp(a)); // reverse sort
-    let sum: u32 = result.iter().take(3).sum();
-    sum
+
+fn parse_input(input: &str) -> HashMap<&str, HashSet<&str>> {
+    let mut adjacency_map: HashMap<&str, HashSet<&str>> = HashMap::new();
+
+    for line in input.lines() {
+        let parts: Vec<&str> = line.split([':', ' ', '\n']).filter(|s| !s.is_empty()).collect();
+
+ 		if let Some(node) = parts.get(0).cloned() {
+            let neighbors: HashSet<&str> = parts[1..].iter().cloned().collect();
+            adjacency_map.insert(node, neighbors);
+        }
+    }
+    adjacency_map
 }
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT : &str = "1
-2
-
-5
-
-1
-
-3
-4";
+    const INPUT : &str = "jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr";
 
     #[test]
     fn part1_works() {
         let result = process_part1(INPUT);
-        assert_eq!(result, 7);
-    }
-
-    #[test]
-    fn part2_works() {
-        let result = process_part2(INPUT);
-        assert_eq!(result, 15);
+        assert_eq!(result, 54);
     }
 }
