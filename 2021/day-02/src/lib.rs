b67e23acc970cc073379da6dcdd35ef0030fd8bb
Kurt Schwind <kurt.schwind@gmail.com>
2023-12-29 03:59:30 -0600

    nom

--- 2021/day-02/src/lib.rs
+++ 2021/day-02/src/lib.rs
@@ -1,48 +1,35 @@
 use nom::character::complete;
-use nom::sequence::separated_pair;
+// use nom::sequence::separated_pair;
 use nom::branch;
 use nom::bytes::complete::tag;
 use nom::IResult;
 
 #[derive(Debug, Clone, Copy)]
 enum Instruction {
-    Forward(usize),
-    Down(usize),
-    Up(usize),
+    Forward(u32),
+    Down(u32),
+    Up(u32),
 }
 
 #[derive(Debug, Clone, Copy)]
 struct Position {
-    depth: usize,
-    distance: usize,
+    depth: u32,
+    distance: u32,
 }
 
-fn forward(input: &str) -> IResult<&str, &str> {
-    tag("forward")(input)
-}
-
-fn down(input: &str) -> IResult<&str, &str> {
-    tag("down")(input)
-}
-
-fn up(input: &str) -> IResult<&str, &str> {
-    tag("up")(input)
-}
-
-fn direction(input: &str) -> IResult<&str, &str> {
-    branch::alt((forward, down, up))(input)
-}
 
 fn parse_line(input: &str) -> IResult<&str, Instruction> {
-    let (input, dir) = branch::alt((forward, down, up))(input);
-    let (input, _) = tag(" ")(input);
-    let (input, length) = character:u32(input)
+    let (input, dir) = branch::alt((tag("forward"), tag("up"), tag("down")))( input,)?;
+    let (input, _) = tag(" ")(input)?;
+    let (input, length) = complete::u32(input)?;
+
     let retval = match dir {
         "forward" => Instruction::Forward(length),
         "down" => Instruction::Down(length),
         "up" => Instruction::Up(length),
         _ => panic!("In parse line, unknown direction"),
     };
+
     Ok((input,retval))
 }
 
@@ -51,14 +38,14 @@ fn parse_input(input: &str) -> IResult<&str, Vec<Instruction>> {
     Ok((input, depths))
 }
 
-pub fn process_part1(input: &str) -> usize {
+pub fn process_part1(input: &str) -> u32 {
     let x : Vec<_> = input.lines().map(parse_line).collect();
     dbg!(&x);
-    input.len()
+    input.len() as u32
 }
 
-pub fn process_part2(input: &str) -> usize {
-    input.len()
+pub fn process_part2(input: &str) -> u32 {
+    input.len() as u32
 }
 
 #[cfg(test)]
