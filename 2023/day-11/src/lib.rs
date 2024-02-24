4b5b7530b511179f80b2b5d94c6349c3306b8f2b
Kurt Schwind <kurt.schwind@gmail.com>
2024-02-24 07:54:04 -0600

    Clippy Fix

--- 2023/day-11/src/lib.rs
+++ 2023/day-11/src/lib.rs
@@ -1,105 +1,3 @@
-/*
-pub fn process_part1(input: &str) -> u32 {
-
-with open("./input.txt") as fin:
-    lines = fin.read().strip().split("\n")
-
-n, m = len(lines), len(lines[0])
-
-coords = []
-for i in range(n):
-    for j in range(m):
-        if lines[i][j] == "#":
-            coords.append((i, j))
-
-N = len(coords)
-
-empty_row = [all([lines[i][j] == "." for j in range(m)]) for i in range(n)]
-empty_col = [all([lines[i][j] == "." for i in range(n)]) for j in range(m)]
-
-
-def dist(a, b):
-    i1, j1 = a
-    i2, j2 = b
-
-    i1, i2 = min(i1, i2), max(i1, i2)
-    j1, j2 = min(j1, j2), max(j1, j2)
-
-    ans = 0
-    for i in range(i1, i2):
-        ans += 1
-        if empty_row[i]:
-            ans += 1
-    for j in range(j1, j2):
-        ans += 1
-        if empty_col[j]:
-            ans += 1
-
-    return ans
-
-
-ans = 0
-for idx1 in range(N):
-    for idx2 in range(idx1+1, N):
-        d = dist(coords[idx1], coords[idx2])
-        ans += d
-
-print(ans)
-    0
-}
-*/
-
-/*
-pub fn process_part2(input: &str) -> u32 {
-
-with open("./input.txt") as fin:
-    lines = fin.read().strip().split("\n")
-
-n, m = len(lines), len(lines[0])
-
-coords = []
-for i in range(n):
-    for j in range(m):
-        if lines[i][j] == "#":
-            coords.append((i, j))
-
-N = len(coords)
-
-empty_row = [all([lines[i][j] == "." for j in range(m)]) for i in range(n)]
-empty_col = [all([lines[i][j] == "." for i in range(n)]) for j in range(m)]
-
-
-def dist(a, b):
-    i1, j1 = a
-    i2, j2 = b
-
-    i1, i2 = min(i1, i2), max(i1, i2)
-    j1, j2 = min(j1, j2), max(j1, j2)
-
-    ans = 0
-    for i in range(i1, i2):
-        ans += 1
-        if empty_row[i]:
-            ans += 10**6 - 1
-    for j in range(j1, j2):
-        ans += 1
-        if empty_col[j]:
-            ans += 10**6 - 1
-
-    return ans
-
-
-ans = 0
-for idx1 in range(N):
-    for idx2 in range(idx1+1, N):
-        d = dist(coords[idx1], coords[idx2])
-        ans += d
-
-print(ans)
-    0
-}
-*/
-
 use itertools::Itertools;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -160,8 +58,8 @@ fn empty_cols(grid: &[Vec<Tile>]) -> Vec<usize> {
 }
 
 fn galaxy_coordinates(grid: &[Vec<Tile>], expansion: usize) -> Vec<Coord> {
-    let empty_rows = empty_rows(&grid);
-    let empty_cols = empty_cols(&grid);
+    let empty_rows = empty_rows(grid);
+    let empty_cols = empty_cols(grid);
 
     let mut galaxies = Vec::new();
     let mut curr_row = 0;
