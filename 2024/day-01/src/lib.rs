74751657268529d91ae300015973ef4cffd2a700
Kurt Schwind <kurt.schwind@gmail.com>
2024-12-01 06:19:23 -0600

    Day 1 complete

--- 2024/day-01/src/lib.rs
+++ 2024/day-01/src/lib.rs
@@ -1,30 +1,40 @@
 pub fn process_part1(input: &str) -> u32 {
-    let result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    result
+    let mut left_list: Vec<u32> = vec![];
+    let mut right_list: Vec<u32> = vec![];
+    for line in input.lines() {
+        let v: Vec<&str> = line.split("   ").collect();
+        let l: u32 = v[0].parse().expect("Parse fail");
+        let r: u32 = v[1].parse().expect("Parse fail");
+        left_list.push(l);
+        right_list.push(r);
+    }
+    left_list.sort();
+    right_list.sort();
+    let mut sum = 0;
+    for i in 0..left_list.len() {
+        sum += left_list[i].abs_diff(right_list[i]);
+    }
+    sum
 }
 
 pub fn process_part2(input: &str) -> u32 {
-    let mut result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
-
-    result.sort_by(|a, b| b.cmp(a)); // reverse sort
-    let sum: u32 = result.iter().take(3).sum();
+    let mut left_list: Vec<u32> = vec![];
+    let mut right_list: Vec<u32> = vec![];
+    for line in input.lines() {
+        let v: Vec<&str> = line.split("   ").collect();
+        let l: u32 = v[0].parse().expect("Parse fail");
+        let r: u32 = v[1].parse().expect("Parse fail");
+        left_list.push(l);
+        right_list.push(r);
+    }
+    left_list.sort();
+    right_list.sort();
+    let mut sum = 0;
+    for i in 0..left_list.len() {
+        let target = left_list[i];
+        let mult = right_list.iter().filter(|x| **x == target).count() as u32;
+        sum += target * mult;
+    }
     sum
 }
 
@@ -32,25 +42,22 @@ pub fn process_part2(input: &str) -> u32 {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "1
-2
-
-5
-
-1
-
-3
-4";
+    const INPUT: &str = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
 
     #[test]
     fn part1_works() {
         let result = process_part1(INPUT);
-        assert_eq!(result, 7);
+        assert_eq!(result, 11);
     }
 
     #[test]
     fn part2_works() {
         let result = process_part2(INPUT);
-        assert_eq!(result, 15);
+        assert_eq!(result, 31);
     }
 }
