2599d19566d7c80152fa021c5720d39bd0876888
Kurt Schwind <kurt.schwind@gmail.com>
2024-12-03 04:57:12 -0600

    Part 1 complete

--- 2024/day-03/src/lib.rs
+++ 2024/day-03/src/lib.rs
@@ -1,15 +1,19 @@
+use regex::Regex;
+
 pub fn process_part1(input: &str) -> u32 {
-    let result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    result
+    let re =
+        Regex::new(r"mul\(([1-9][0-9]?[0-9]?),([1-9][0-9]?[0-9]?)\)").expect("Poorly formed regex");
+
+    let mut results = vec![];
+    for (_, [a, b]) in re.captures_iter(input).map(|c| c.extract()) {
+        results.push((a, b));
+    }
+
+    // println!("{:?}", &results);
+    results
+        .iter()
+        .map(|(a, b)| a.parse::<u32>().unwrap() * b.parse::<u32>().unwrap())
+        .sum()
 }
 
 pub fn process_part2(input: &str) -> u32 {
@@ -32,25 +36,19 @@ pub fn process_part2(input: &str) -> u32 {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "1
-2
-
-5
-
-1
-
-3
-4";
+    const INPUT1: &str = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+    const INPUT2: &str =
+        "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
 
     #[test]
     fn part1_works() {
-        let result = process_part1(INPUT);
-        assert_eq!(result, 7);
+        let result = process_part1(INPUT1);
+        assert_eq!(result, 161);
     }
 
     #[test]
     fn part2_works() {
-        let result = process_part2(INPUT);
-        assert_eq!(result, 15);
+        let result = process_part2(INPUT2);
+        assert_eq!(result, 48);
     }
 }
