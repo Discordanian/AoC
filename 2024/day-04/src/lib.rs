4f0d0cd8ed13b408c2d41622c34c908e5822c310
Kurt Schwind <kurt.schwind@gmail.com>
2024-12-04 04:19:02 -0600

    Complete

--- 2024/day-04/src/lib.rs
+++ 2024/day-04/src/lib.rs
@@ -1,56 +1,94 @@
+use std::collections::HashMap;
+
 pub fn process_part1(input: &str) -> u32 {
-    let result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    result
+    let mut hm = HashMap::new();
+
+    for (row, line) in input.lines().enumerate() {
+        for (col, c) in line.chars().enumerate() {
+            hm.insert((row as i32, col as i32), c);
+        }
+    }
+
+    let mut dirs: Vec<Vec<(i32, i32)>> = vec![];
+    dirs.push(vec![(0, 1), (0, 2), (0, 3)]);
+    dirs.push(vec![(1, 1), (2, 2), (3, 3)]);
+    dirs.push(vec![(1, 0), (2, 0), (3, 0)]);
+    dirs.push(vec![(-1, 0), (-2, 0), (-3, 0)]);
+    dirs.push(vec![(0, -1), (0, -2), (0, -3)]);
+    dirs.push(vec![(1, -1), (2, -2), (3, -3)]);
+    dirs.push(vec![(-1, -1), (-2, -2), (-3, -3)]);
+    dirs.push(vec![(-1, 1), (-2, 2), (-3, 3)]);
+
+    let mut retval = 0;
+    for (k, v) in hm.clone().into_iter() {
+        if v == 'X' {
+            for d in dirs.iter() {
+                let m = hm.get(&(k.0 + d[0].0, k.1 + d[0].1));
+                let a = hm.get(&(k.0 + d[1].0, k.1 + d[1].1));
+                let s = hm.get(&(k.0 + d[2].0, k.1 + d[2].1));
+                if m == Some(&'M') && a == Some(&'A') && s == Some(&'S') {
+                    retval += 1;
+                }
+            }
+        }
+    }
+
+    retval
 }
 
 pub fn process_part2(input: &str) -> u32 {
-    let mut result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
+    let mut hm = HashMap::new();
 
-    result.sort_by(|a, b| b.cmp(a)); // reverse sort
-    let sum: u32 = result.iter().take(3).sum();
-    sum
+    for (row, line) in input.lines().enumerate() {
+        for (col, c) in line.chars().enumerate() {
+            hm.insert((row as i32, col as i32), c);
+        }
+    }
+
+    let mut retval = 0;
+    for (k, v) in hm.clone().into_iter() {
+        if v == 'A' {
+            let c1 = hm.get(&(k.0 + 1, k.1 + 1));
+            let c2 = hm.get(&(k.0 - 1, k.1 - 1));
+            let c3 = hm.get(&(k.0 + 1, k.1 - 1));
+            let c4 = hm.get(&(k.0 - 1, k.1 + 1));
+            let diag1: bool =
+                (c1 == Some(&'M') && c2 == Some(&'S')) || (c1 == Some(&'S') && c2 == Some(&'M'));
+            let diag2: bool =
+                (c3 == Some(&'M') && c4 == Some(&'S')) || (c3 == Some(&'S') && c4 == Some(&'M'));
+            if diag1 && diag2 {
+                retval += 1;
+            }
+        }
+    }
+
+    retval
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "1
-2
-
-5
-
-1
-
-3
-4";
+    const INPUT: &str = "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX";
 
     #[test]
     fn part1_works() {
         let result = process_part1(INPUT);
-        assert_eq!(result, 7);
+        assert_eq!(result, 18);
     }
 
     #[test]
     fn part2_works() {
         let result = process_part2(INPUT);
-        assert_eq!(result, 15);
+        assert_eq!(result, 9);
     }
 }
