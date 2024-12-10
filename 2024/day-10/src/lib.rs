18fbc11c4c3bc01653837ba9c6a8fb1aaf744560
Kurt Schwind <kurt.schwind@gmail.com>
2024-12-10 12:14:51 -0600

    In progress

--- 2024/day-10/src/lib.rs
+++ 2024/day-10/src/lib.rs
@@ -1,56 +1,54 @@
-pub fn process_part1(input: &str) -> u32 {
-    let result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .max()
-        .unwrap();
-    result
+use std::collections::{BTreeMap, BTreeSet};
+
+pub fn trail_heads(grid: &[Vec<i32>]) -> BTreeSet<(i32, i32)> {
+    let mut retval = BTreeSet::new();
+    for (row, line) in grid.iter().enumerate() {
+        for (col, height) in line.iter().enumerate() {
+            if *height == 0 {
+                retval.insert((row as i32, col as i32));
+            }
+        }
+    }
+    retval
 }
 
-pub fn process_part2(input: &str) -> u32 {
-    let mut result = input
-        .split("\n\n") // Empty line between records
-        .map(|record| {
-            record
-                .lines()
-                .map(|row| row.parse::<u32>().unwrap())
-                .sum::<u32>()
-        })
-        .collect::<Vec<_>>();
+pub fn process_part1(input: &str) -> u64 {
+    let grid: Vec<Vec<i32>> = input
+        .lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).unwrap() as i32).collect())
+        .collect::<Vec<Vec<i32>>>();
 
-    result.sort_by(|a, b| b.cmp(a)); // reverse sort
-    let sum: u32 = result.iter().take(3).sum();
-    sum
+    let heads = trail_heads(&grid);
+    // dbg!(&heads);
+    grid.len() as u64
+}
+
+pub fn process_part2(_input: &str) -> u64 {
+    0
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "1
-2
-
-5
-
-1
-
-3
-4";
+    const INPUT: &str = "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732";
 
     #[test]
     fn part1_works() {
         let result = process_part1(INPUT);
-        assert_eq!(result, 7);
+        assert_eq!(result, 36);
     }
 
     #[test]
     fn part2_works() {
         let result = process_part2(INPUT);
-        assert_eq!(result, 15);
+        assert_eq!(result, 0);
     }
 }
