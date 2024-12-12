1757fc2c29c6f334befef769a660df6214872506
Kurt Schwind <kurt.schwind@gmail.com>
2024-12-12 12:25:35 -0600

    2016

---
+++ 2016/day-01/get_input.sh
@@ -0,0 +1,5 @@
+YEAR=2016
+((DAY=$1+0))
+
+echo curl -b session=`cat session_id` https://adventofcode.com/$YEAR/day/$DAY/input 
+curl -b session=`cat session_id` https://adventofcode.com/$YEAR/day/$DAY/input > input.txt
---
+++ 2016/day-11/src/bin/solution.rs
@@ -0,0 +1,9 @@
+use day_11::process_part1;
+use day_11::process_part2;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("./input.txt").unwrap();
+    println!("Day Part 1 answer: {}", process_part1(&file));
+    println!("Day Part 2 answer: {}", process_part2(&file));
+}
