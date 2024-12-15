6ac9f3f286dae88398cee824d3009ada1295f118
Kurt Schwind <kurt.schwind@gmail.com>
2024-12-15 15:01:29 -0600

    Day 2

--- 2024/day-15/src/lib.rs
+++ 2024/day-15/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 // I like BTree over Hash because it makes debugging easier
 // If you want speed, use Hash instead
 
@@ -29,10 +29,52 @@ pub fn bot_position(g: &BTreeMap<(i32, i32), char>) -> (i32, i32) {
     (0, 0)
 }
 
+pub fn bot_position2(g: &[Vec<char>]) -> (usize, usize) {
+    for (row, line) in g.iter().enumerate() {
+        for (col, c) in line.iter().enumerate() {
+            if *c == '@' {
+                return (row, col);
+            }
+        }
+    }
+    (0, 0)
+}
+
+pub fn make_fat_grid(input: &str) -> Vec<Vec<char>> {
+    input
+        .lines()
+        .map(|line| {
+            let mut row_vec: Vec<char> = Vec::new();
+            for c in line.chars() {
+                match c {
+                    '.' => {
+                        row_vec.push('.');
+                        row_vec.push('.');
+                    }
+                    'O' => {
+                        row_vec.push('[');
+                        row_vec.push(']');
+                    }
+                    '#' => {
+                        row_vec.push('#');
+                        row_vec.push('#');
+                    }
+                    '@' => {
+                        row_vec.push('@');
+                        row_vec.push('.');
+                    }
+                    _ => panic!("Unknown character received while creating fat grid"),
+                }
+            }
+            row_vec
+        })
+        .collect()
+}
+
 pub fn process_part1(input: &str) -> i32 {
     let (g, i) = input.split_once("\n\n").unwrap();
 
-    let mut grid = make_grid(g);
+    let mut grid = make_grid1(g);
     let instructions = make_instructions(i);
     let mut botpos = bot_position(&grid);
 
@@ -106,8 +148,103 @@ pub fn score_part1(grid: BTreeMap<(i32, i32), char>) -> i32 {
         .sum::<i32>()
 }
 
-pub fn process_part2(input: &str) -> u32 {
-    input.len() as u32
+pub fn score_part2(boxes: Vec<(usize, usize)>) -> usize {
+    boxes.iter().map(|key| key.0 * 100 + key.1).sum()
+}
+
+pub fn pair_addition(p: (usize, usize), delta: (i32, i32)) -> (usize, usize) {
+    assert!(p.0 > 0);
+    assert!(p.1 > 0);
+    assert!(delta.0.abs() < 2);
+    let x = ((p.0 as i32) + delta.0) as usize;
+    let y = ((p.1 as i32) + delta.1) as usize;
+    (x, y)
+}
+
+pub fn wall_set(grid: &[Vec<char>]) -> BTreeSet<(usize, usize)> {
+    let mut retval: BTreeSet<_> = BTreeSet::new();
+    for (row, line) in grid.iter().enumerate() {
+        for (col, c) in line.iter().enumerate() {
+            if *c == '#' {
+                retval.insert((row, col));
+            }
+        }
+    }
+    retval
+}
+
+pub fn vec_boxes(grid: &[Vec<char>]) -> Vec<(usize, usize)> {
+    let mut retval = Vec::new();
+    for (row, line) in grid.iter().enumerate() {
+        for (col, c) in line.iter().enumerate() {
+            if *c == '[' {
+                retval.push((row, col));
+            }
+        }
+    }
+    retval
+}
+
+pub fn process_part2(input: &str) -> usize {
+    let (g, i) = input.split_once("\n\n").unwrap();
+
+    let grid = make_fat_grid(g);
+    let wallset = wall_set(&grid);
+    let mut boxlist = vec_boxes(&grid);
+
+    let instructions = make_instructions(i);
+    let mut botpos = bot_position2(&grid);
+
+    for dir in instructions.iter() {
+        let delta: (i32, i32) = match dir {
+            '^' => (-1, 0),
+            'v' => (1, 0),
+            '>' => (0, 1),
+            '<' => (0, -1),
+            _ => panic!("At the disco"),
+        };
+
+        let mut canmove = true;
+        let mut stack: Vec<(usize, usize)> = vec![];
+
+        let proposed = pair_addition(botpos, delta);
+        if wallset.contains(&proposed) {
+            continue;
+        }
+
+        if boxlist.contains(&proposed) {
+            stack.push(proposed);
+        }
+        if boxlist.contains(&pair_addition(proposed, (0, -1))) {
+            stack.push(pair_addition(proposed, (0, -1)));
+        }
+
+        let mut seen: BTreeSet<(usize, usize)> = BTreeSet::new();
+
+        while let Some(next) = stack.pop() {
+            let proposed = pair_addition(next, delta);
+            let proposed_wide = (proposed.0, proposed.1 + 1); // box is 2 wide
+
+            if wallset.contains(&proposed) || wallset.contains(&proposed_wide) {
+                canmove = false;
+                break;
+            }
+
+            if seen.contains(&proposed) {
+                continue;
+            }
+            seen.insert(proposed);
+        } // stack processing
+        if canmove {
+            botpos = pair_addition(botpos, delta);
+            for i in 0..boxlist.len() {
+                if seen.contains(&boxlist[i]) {
+                    boxlist[i] = pair_addition(boxlist[i], delta);
+                }
+            }
+        }
+    }
+    score_part2(boxlist)
 }
 
 #[cfg(test)]
