230c9f00b183b241ebdb17d5f210f4cd13a17128
Kurt Schwind <kurt.schwind@gmail.com>
2024-12-25 04:23:23 -0600

    Merry Christmas!  Day 25 Complete

--- 2024/day-25/src/lib.rs
+++ 2024/day-25/src/lib.rs
@@ -1,26 +1,88 @@
-pub fn process_part1(input: &str) -> u32 {
-    0
-}
+use std::collections::HashSet;
 
-pub fn process_part2(input: &str) -> u32 {
-    0
+pub fn section_to_set(input: &str) -> std::collections::HashSet<(usize, usize)> {
+    input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars()
+                .enumerate()
+                .filter(|(_, val)| *val == '#')
+                .map(move |(x, _)| (x, y)) //move required for y value in closure
+        })
+        .collect()
+}
+pub fn process_part1(input: &str) -> usize {
+    let mut locks: Vec<HashSet<(usize, usize)>> = Vec::new();
+    let mut keys: Vec<HashSet<(usize, usize)>> = Vec::new();
+    let mut retval = 0;
+
+    for section in input.split("\n\n") {
+        let set = section_to_set(section);
+        if set.contains(&(0, 0)) {
+            locks.push(set);
+        } else {
+            keys.push(set);
+        }
+    }
+    for key in keys.iter() {
+        for lock in locks.iter() {
+            if key.intersection(lock).count() == 0 {
+                retval += 1;
+            }
+        }
+    }
+
+    retval
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "#####
+.####
+.####
+.####
+.#.#.
+.#...
+.....
+
+#####
+##.##
+.#.##
+...##
+...#.
+...#.
+.....
+
+.....
+#....
+#....
+#...#
+#.#.#
+#.###
+#####
+
+.....
+.....
+#.#..
+###..
+###.#
+###.#
+#####
+
+.....
+.....
+.....
+#....
+#.#..
+#.#.#
+#####";
 
     #[test]
     fn part1_works() {
         let result = process_part1(INPUT);
-        assert_eq!(result, 0);
-    }
-
-    #[test]
-    fn part2_works() {
-        let result = process_part2(INPUT);
-        assert_eq!(result, 0);
+        assert_eq!(result, 3);
     }
 }
