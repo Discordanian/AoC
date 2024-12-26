15c1c965d56d91942856328f7f8e6f64ab6c0856
Kurt Schwind <kurt.schwind@gmail.com>
2024-12-26 08:34:03 -0600

    2021

--- 2021/advent.sh
+++ 2021/advent.sh
@@ -17,12 +17,11 @@ git add $LIBNAME
 cp $SRCLIB/solve.sh $LIBNAME/solve.sh
 mkdir $LIBNAME/src/bin/
 sed "s/00/$1/g" $SRCLIB/src/bin/part-1.rs > $LIBNAME/src/bin/part-1.rs
-sed "s/00/$1/g" $SRCLIB/src/bin/part-2.rs > $LIBNAME/src/bin/part-2.rs
+sed "s/00/$1/g" $SRCLIB/src/bin/part-2.rs $LIBNAME/src/bin/part-2.rs
 cp -v $SRCLIB/src/lib.rs $LIBNAME/src/lib.rs
+cp -v $SRCLIB/get_input.sh $LIBNAME/
 
 cd $LIBNAME
-cargo add itertools
-cargo add nom
 cargo test
 cargo clean
 cd ..
--- 2021/day-00/day-00/src/lib.rs
+++ 2021/day-00/day-00/src/lib.rs
@@ -1,9 +1,9 @@
-pub fn process_part1(input: &str) -> usize {
-    input.len()
+pub fn process_part1(input: &str) -> u32 {
+    input.len() as u32
 }
 
-pub fn process_part2(input: &str) -> usize {
-    input.len()
+pub fn process_part2(input: &str) -> u32 {
+    input.len() as u32
 }
 
 #[cfg(test)]
@@ -13,13 +13,13 @@ mod tests {
     const INPUT: &str = "";
 
     #[test]
-    fn part1_example() {
+    fn part1_works() {
         let result = process_part1(INPUT);
         assert_eq!(result, 0);
     }
 
     #[test]
-    fn part2_example() {
+    fn part2_works() {
         let result = process_part2(INPUT);
         assert_eq!(result, 0);
     }
