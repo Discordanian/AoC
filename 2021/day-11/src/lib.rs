18b94ba941fb77b98ceb7432e413e2ddef23df57
Kurt Schwind <kurt.schwind@gmail.com>
2024-12-27 12:47:30 -0600

    day-11

---
+++ 2021/day-11/src/lib.rs
@@ -0,0 +1,26 @@
+pub fn process_part1(input: &str) -> usize {
+    input.len()
+}
+
+pub fn process_part2(input: &str) -> usize {
+    input.len()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn part1_example() {
+        let result = process_part1(INPUT);
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn part2_example() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, 0);
+    }
+}
