677871b33ba34ab22fa7c4181cfb2b754eed97ae
Kurt Schwind <kurt.schwind@gmail.com>
2025-01-08 14:12:54 -0600

    Year 2019 Day 1

--- 2019/day-01/src/lib.rs
+++ 2019/day-01/src/lib.rs
@@ -1,9 +1,28 @@
-pub fn process_part1(input: &str) -> u32 {
-    input.len() as u32
+pub fn fuel_required(mass: i32) -> i32 {
+    (mass / 3) - 2
 }
 
-pub fn process_part2(input: &str) -> u32 {
-    input.len() as u32
+pub fn fuel_required_recursive(mass: i32) -> i32 {
+    let fuel = (mass / 3) - 2;
+    if fuel <= 0 {
+        0
+    } else {
+        fuel + fuel_required_recursive(fuel)
+    }
+}
+
+pub fn process_part1(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|mass| fuel_required(mass.parse::<i32>().expect("Able to parse to i32")))
+        .sum()
+}
+
+pub fn process_part2(input: &str) -> i32 {
+    input
+        .lines()
+        .map(|mass| fuel_required_recursive(mass.parse::<i32>().expect("Able to parse to i32")))
+        .sum()
 }
 
 #[cfg(test)]
@@ -35,9 +54,20 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
-    fn part2_works() {
-        let result = process_part2(INPUT);
-        assert_eq!(result, 15);
+    fn part2a_works() {
+        let result = process_part2("14");
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    fn part2b_works() {
+        let result = process_part2("1969");
+        assert_eq!(result, 966);
+    }
+
+    #[test]
+    fn part2c_works() {
+        let result = process_part2("100756");
+        assert_eq!(result, 50346);
     }
 }
