5761ce0795219eefea5eea581e9d9f39564a3660
Kurt Schwind <kurt.schwind@gmail.com>
2025-10-12 10:16:17 -0500

    Initial button layout

---
+++ 2025/advent-of-code-2025/scripts/day_button.gd
@@ -0,0 +1,14 @@
+extends Button
+class_name DayButton
+
+var day: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    pass # Replace with function body.
+
+
+func setup(d: int) -> void:
+    print("Setup called with ", d)
+    $".".text = "Day %02d" % d
+    print(text)
---
+++ 2025/advent-of-code-2025/scripts/main_entry.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var grid: GridContainer = $MarginContainer/VBoxContainer/ButtonGrid
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    for i in range(25):
+        print("Button Loop ", i)
+        var b = DayButton.new()
+        b.setup(i+1)
+        grid.add_child(b)
+        
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    pass
