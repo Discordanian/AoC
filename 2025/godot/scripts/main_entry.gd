82cc1fd4e59746564a2f5112a23e8bdf32d63eae
Kurt Schwind <kurt.schwind@gmail.com>
2025-10-15 05:56:56 -0500

    Saving your bacon

--- 2025/godot/scripts/main_entry.gd
+++ 2025/godot/scripts/main_entry.gd
@@ -4,15 +4,10 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    for i: int in range(25):
-        # print("Button Loop ", i)
-        var b: DayButton = DayButton.new()
-        b.disabled = true
-        b.setup(i+1)
-        grid.add_child(b)
-        
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-    pass
+	for i: int in range(25):
+		# print("Button Loop ", i)
+		var b: DayButton = DayButton.new()
+		b.disabled = true
+		b.setup(i+1)
+		grid.add_child(b)
+		
