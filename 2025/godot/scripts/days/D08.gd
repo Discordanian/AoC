b70a6869f15668df780223cab49cc8799b3d36ad
Kurt Schwind <kurt.schwind@gmail.com>
2025-12-08 06:08:19 -0600

    Saving your bacon

--- 2025/godot/scripts/days/D08.gd
+++ 2025/godot/scripts/days/D08.gd
@@ -1,8 +1,79 @@
 class_name D08 extends RefCounted
 
+var a1: int = 0
+var a2: int = 0
+
+func both(data: String) -> void:
+	var P: Array[Vector3i] = []
+	var lines: PackedStringArray = data.split("\n")
+	
+	for line: String in lines:
+		var s: String = line.strip_edges()
+		if s.is_empty():
+			continue
+		var numbers: PackedStringArray = s.split(",", false)
+		assert(numbers.size() == 3)
+		
+		P.append(Vector3i(numbers[0].to_int(), numbers[1].to_int(), numbers[2].to_int()))
+	
+	var n: int = P.size()
+	assert(n>0)
+	
+	var D: Array[Array] = []
+	
+	for i: int in range(n):
+		for j: int in range(n):
+			if i > j:
+				var dx: int = P[i].x - P[j].x
+				var dy: int = P[i].y - P[j].y
+				var dz: int = P[i].z - P[j].z
+				var dist: int = dx * dx + dy * dy + dz * dz
+				var edge: Array[int] = [dist, i, j]
+				D.append(edge)
+	
+	D.sort()
+	
+	var uf: UnionFind = UnionFind.new(n)
+	var connections: int = 0
+	
+	for t: int in range(D.size()):
+		var edge: Array[int] = D[t]
+		var i: int = edge[1]
+		var j: int = edge[2]
+		
+		if t == 1000:
+			print("In t 1000")
+			var sizes: Dictionary = {}
+			for x: int in range(n):
+				var r: int = uf.find(x)
+				var prev: int = int(sizes.get(r,0))
+				sizes[r] = prev + 1
+			
+			var s_arr: Array[int] = []
+			for v: int in sizes.values():
+				s_arr.append(v)
+			s_arr.sort()
+			
+			assert(s_arr.size() > 3)
+			
+			var last_idx: int = s_arr.size() - 1
+			a1 = s_arr[last_idx] * s_arr[last_idx - 1] * s_arr[last_idx - 2]
+		
+		if uf.find(i) != uf.find(j):
+			connections += 1
+			
+			if connections == (n-1):
+				a2 = P[i].x * P[j].x
+			
+			uf.unite(i,j)
+			
+		
+	
 
 func part1(data: String) -> String:
-    return data
-    
+	both(data)
+	return str(a1)
+	
 func part2(data: String) -> String:
-    return data
+	both(data)
+	return str(a2)
--- 2025/godot/singletons/AOCUtil.gd
+++ 2025/godot/singletons/AOCUtil.gd
@@ -20,129 +20,129 @@ const ALL_NEIGHBORS: Array[Vector2i] = [ NORTH, NORTHEAST, EAST, SOUTHEAST, SOUT
 ## @param d int The day
 ## @return String Returns a String path to the resource file for example
 static func example_path1(y: int, d: int) -> String:
-    assert(y > 2014, "Year must be > 2014")
-    assert(y < 2026, "Year must be < 2026")
-    assert(d > - 1, "Day must not be negative")
-    assert(d < 26, "Advent of code does not allow days greater than 25")
-    return "user://%4d-%02d.ex1" % [y,d]
+	assert(y > 2014, "Year must be > 2014")
+	assert(y < 2026, "Year must be < 2026")
+	assert(d > - 1, "Day must not be negative")
+	assert(d < 26, "Advent of code does not allow days greater than 25")
+	return "user://%4d-%02d.ex1" % [y,d]
 
 ## Example path given year and day
 ## @param y int The year
 ## @param d int The day
 ## @return String Returns a String path to the resource file for example
 static func example_path2(y: int, d: int) -> String:
-    assert(y > 2014, "Year must be > 2014")
-    assert(y < 2026, "Year must be < 2026")
-    assert(d > - 1, "Day must not be negative")
-    assert(d < 26, "Advent of code does not allow days greater than 25")
-    return "user://%4d-%02d.ex2" % [y,d]
+	assert(y > 2014, "Year must be > 2014")
+	assert(y < 2026, "Year must be < 2026")
+	assert(d > - 1, "Day must not be negative")
+	assert(d < 26, "Advent of code does not allow days greater than 25")
+	return "user://%4d-%02d.ex2" % [y,d]
 
 ## Input path given year and day
 ## @param y int The year
 ## @param d int The day
 ## @return String Returns a String path to the resource file for the input
 static func input_path(y: int, d: int) -> String:
-    assert(y > 2014, "Year must be > 2014")
-    assert(y < 2026, "Year must be < 2026")
-    assert(d > - 1, "Day must not be negative")
-    assert(d < 26, "Advent of code does not allow days greater than 25")
-    return "user://%4d-%02d.in" % [y,d]
+	assert(y > 2014, "Year must be > 2014")
+	assert(y < 2026, "Year must be < 2026")
+	assert(d > - 1, "Day must not be negative")
+	assert(d < 26, "Advent of code does not allow days greater than 25")
+	return "user://%4d-%02d.in" % [y,d]
 
 
 ## Get the dimensions of an array of an array (Assumes col count is same for all rows)
 ## @param data The 2d array
 ## @returns Vector2i with x being the row count and y being the col count
 static func dimensions_of_2d_array(data: Array[Array]) -> Vector2i:
-    var x: int = data.size()
-    if x > 0:
-        return Vector2i(x, data[0].size())
-    return Vector2i(0,0)
+	var x: int = data.size()
+	if x > 0:
+		return Vector2i(x, data[0].size())
+	return Vector2i(0,0)
 
 
 ## Takes an input string with newlines and returns a 2d character array
 ## @param input a string to parse
 ## @returns Array of Array of characters
 static func string_to_2d_char_array(input: String) -> Array[Array]:
-    var result: Array[Array] = []
-    var lines: PackedStringArray = input.split("\n")
-    
-    for line: String in lines:
-        var char_array: Array[String] = []
-        for i: int in range(line.length()):
-            char_array.append(line[i])
-        result.append(char_array)
-    
-    return result
+	var result: Array[Array] = []
+	var lines: PackedStringArray = input.split("\n")
+	
+	for line: String in lines:
+		var char_array: Array[String] = []
+		for i: int in range(line.length()):
+			char_array.append(line[i])
+		result.append(char_array)
+	
+	return result
 
 
 ## Converts a single string to an array of Strings (split on newline)
 ## @param input: String
 ## @returns PackedStringArray An array of strings
 static func string_to_lines(input: String) -> PackedStringArray:
-    return  input.split("\n")
+	return  input.split("\n")
 
 
 ## Given a string returns an array of an array of numbers
 ## @param input The input string.
 ## @returns An array of array of integers.  Both positive or negative. 
 static func string_to_2d_int_array(input: String) -> Array[Array]:
-    var result: Array[Array] = []
-    var lines: PackedStringArray = input.split("\n")
-    
-    for line: String in lines:
-        if line.is_empty():
-            continue
-        
-        var numbers: Array[int] = []
-        var current_number: String = ""
-        var is_negative: bool = false
-        
-        for i: int in range(line.length()):
-            var c: String = line[i]
-            
-            # Check if it's a digit
-            if c.is_valid_int():
-                current_number += c
-            # Check if it's a negative sign at the start of a number
-            elif c == "-" and current_number.is_empty():
-                is_negative = true
-            else:
-                # Non-numeric character - save current number if exists
-                if not current_number.is_empty():
-                    var num: int = int(current_number)
-                    if is_negative:
-                        num = -num
-                    numbers.append(num)
-                    current_number = ""
-                    is_negative = false
-        
-        # Don't forget the last number in the line
-        if not current_number.is_empty():
-            var num: int = int(current_number)
-            if is_negative:
-                num = -num
-            numbers.append(num)
-        
-        if numbers.size() > 0:
-            result.append(numbers)
-    
-    return result
-    
+	var result: Array[Array] = []
+	var lines: PackedStringArray = input.split("\n")
+	
+	for line: String in lines:
+		if line.is_empty():
+			continue
+		
+		var numbers: Array[int] = []
+		var current_number: String = ""
+		var is_negative: bool = false
+		
+		for i: int in range(line.length()):
+			var c: String = line[i]
+			
+			# Check if it's a digit
+			if c.is_valid_int():
+				current_number += c
+			# Check if it's a negative sign at the start of a number
+			elif c == "-" and current_number.is_empty():
+				is_negative = true
+			else:
+				# Non-numeric character - save current number if exists
+				if not current_number.is_empty():
+					var num: int = int(current_number)
+					if is_negative:
+						num = -num
+					numbers.append(num)
+					current_number = ""
+					is_negative = false
+		
+		# Don't forget the last number in the line
+		if not current_number.is_empty():
+			var num: int = int(current_number)
+			if is_negative:
+				num = -num
+			numbers.append(num)
+		
+		if numbers.size() > 0:
+			result.append(numbers)
+	
+	return result
+	
 ## Dumps contents of file to a String
 ## @param path String Path to file to read from
 ## @returns String Contents of file
 static func string_from_file(path: String) -> String:
-    var retval: String = ""
-    if FileAccess.file_exists(path):
-        var file: FileAccess = FileAccess.open(path, FileAccess.READ)
-        if file:
-            retval = file.get_as_text()
-            file.close()
-        else:
-            push_error("Error reading data from " + path)
-    else:
-        push_warning("Path not found : " + path)
-    return retval.strip_edges()
+	var retval: String = ""
+	if FileAccess.file_exists(path):
+		var file: FileAccess = FileAccess.open(path, FileAccess.READ)
+		if file:
+			retval = file.get_as_text()
+			file.close()
+		else:
+			push_error("Error reading data from " + path)
+	else:
+		push_warning("Path not found : " + path)
+	return retval.strip_edges()
 
 ## Downloads the input file from Advent of Code
 ## @param parent_node This requires a parent node to attach to while the http_request occurs, normal `self`
@@ -151,99 +151,99 @@ static func string_from_file(path: String) -> String:
 ## @param file_path The file path to save the downloaded contents to
 ## @returns void
 static func download_file(parent_node: Node, year: int, day: int, file_path: String) -> void:
-    # Get session ID from environment variable
-    var session_id: String = OS.get_environment("SESSIONID")
-    
-    if session_id.is_empty():
-        push_error("SESSIONID environment variable not found")
-        return
-    
-    # Create HTTPRequest node
-    var http_request: HTTPRequest = HTTPRequest.new()
-    parent_node.add_child(http_request)
-    
-    # Build the URL
-    var url: String = "https://adventofcode.com/%d/day/%d/input" % [year, day]
-    
-    # Set up headers with cookie
-    var headers: Array[String] = ["Cookie: session=%s" % session_id]
-    
-    # Connect with a lambda that captures http_request and file_path
-    http_request.request_completed.connect(func(_result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
-        if response_code == 200:
-            var content: String = body.get_string_from_utf8().strip_edges()
-            print("Downloaded successfully!")
-            
-            var file: FileAccess = FileAccess.open(file_path, FileAccess.WRITE)
-            if file:
-                file.store_string(content)
-                file.close()
-                print("Saved to: %s" % file_path)
-            else:
-                push_error("Failed to open file for writing: %s" % file_path)
-        else:
-            push_error("HTTP request [%s] failed with response code: %d" % [url, response_code])
-        
-        # Remove and free the HTTPRequest node
-        http_request.queue_free()
-    )
-    
-    # Make the request
-    var error: Error = http_request.request(url, headers)
-    
-    if error != OK:
-        push_error("An error occurred in the HTTP request.")
+	# Get session ID from environment variable
+	var session_id: String = OS.get_environment("SESSIONID")
+	
+	if session_id.is_empty():
+		push_error("SESSIONID environment variable not found")
+		return
+	
+	# Create HTTPRequest node
+	var http_request: HTTPRequest = HTTPRequest.new()
+	parent_node.add_child(http_request)
+	
+	# Build the URL
+	var url: String = "https://adventofcode.com/%d/day/%d/input" % [year, day]
+	
+	# Set up headers with cookie
+	var headers: Array[String] = ["Cookie: session=%s" % session_id]
+	
+	# Connect with a lambda that captures http_request and file_path
+	http_request.request_completed.connect(func(_result: int, response_code: int, _headers: PackedStringArray, body: PackedByteArray) -> void:
+		if response_code == 200:
+			var content: String = body.get_string_from_utf8().strip_edges()
+			print("Downloaded successfully!")
+			
+			var file: FileAccess = FileAccess.open(file_path, FileAccess.WRITE)
+			if file:
+				file.store_string(content)
+				file.close()
+				print("Saved to: %s" % file_path)
+			else:
+				push_error("Failed to open file for writing: %s" % file_path)
+		else:
+			push_error("HTTP request [%s] failed with response code: %d" % [url, response_code])
+		
+		# Remove and free the HTTPRequest node
+		http_request.queue_free()
+	)
+	
+	# Make the request
+	var error: Error = http_request.request(url, headers)
+	
+	if error != OK:
+		push_error("An error occurred in the HTTP request.")
 
 ## Transpose 2d array
 ## @param data Array[Array]
 ## @returns Array[Array] that converts rows and cols
 static func transpose_2d_array(data: Array[Array]) -> Array[Array]:
-    if data.is_empty() or data[0].is_empty():
-        return []
+	if data.is_empty() or data[0].is_empty():
+		return []
 
-    var rows: int = data.size()
-    var cols: int = data[0].size()
-    var retval: Array[Array] = []
+	var rows: int = data.size()
+	var cols: int = data[0].size()
+	var retval: Array[Array] = []
 
-    for col: int in range(cols):
-        var new_row: Array = []
-        for row: int in range(rows):
-            new_row.append(data[row][col])
-        retval.append(new_row)
-    
-    return retval
+	for col: int in range(cols):
+		var new_row: Array = []
+		for row: int in range(rows):
+			new_row.append(data[row][col])
+		retval.append(new_row)
+	
+	return retval
 
 ## Rotate 2D array 90 degrees clockwise
 ## @param data 2d Array
 ## @returns new rotated array
 static func rotate_2d_array_clockwise(data: Array[Array]) -> Array[Array]:
-    if data.is_empty() or data[0].is_empty():
-        return []
+	if data.is_empty() or data[0].is_empty():
+		return []
 
-    var rows: int = data.size()
-    var cols: int = data[0].size()
-    var retval: Array[Array] = []
+	var rows: int = data.size()
+	var cols: int = data[0].size()
+	var retval: Array[Array] = []
 
-    for col: int in range(cols):
-        var new_row: Array = []
-        for row: int in range(rows - 1, -1, -1):
-            new_row.append(data[row][col])
-        retval.append(new_row)
+	for col: int in range(cols):
+		var new_row: Array = []
+		for row: int in range(rows - 1, -1, -1):
+			new_row.append(data[row][col])
+		retval.append(new_row)
 
-    return retval
+	return retval
 
 ## Returns array of integers from a string
 ## @param data String
 ## @returns Array[int]
 static func array_int_from_string(data: String) -> Array[int]:
-    var regex: RegEx = RegEx.new()
-    regex.compile(r"\d+")
-    
-    var matches: Array[RegExMatch] = regex.search_all(data)
-    var retval: Array[int] = []
-    
-    for match: RegExMatch in matches:
-        var number: int = match.get_string().to_int()
-        retval.append(number)
-        
-    return retval
+	var regex: RegEx = RegEx.new()
+	regex.compile(r"\d+")
+	
+	var matches: Array[RegExMatch] = regex.search_all(data)
+	var retval: Array[int] = []
+	
+	for match: RegExMatch in matches:
+		var number: int = match.get_string().to_int()
+		retval.append(number)
+		
+	return retval
