bb535465219674e818f0c3051aa07aa726325905
Kurt Schwind <kurt.schwind@gmail.com>
2026-01-28 11:27:32 -0600

    dev

--- 2025/dev.sh
+++ 2025/dev.sh
@@ -7,7 +7,7 @@ fi
 
 
 git pull
-export BIN="/Applications/Godot v4.5.app/Contents/MacOS/Godot"
+export BIN="/Applications/Godot v4.6.app/Contents/MacOS/Godot"
 echo $BIN godot/project.godot
 "$BIN" --verbose godot/project.godot
 git add godot && git commit -a -m "Saving your bacon" && git push
